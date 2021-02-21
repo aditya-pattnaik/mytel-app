b73831c98b55a482aa79fc5a7756f07dbcd99d81
aditya <pattnaikaditya1@gmail.com>
2021-02-22 00:53:21 +0530

    Configured spring cloud bus using rabbitmq

--- mytel-friend-family/src/main/java/com/test/mytel/friend/FriendFamilyApplication.java
+++ mytel-friend-family/src/main/java/com/test/mytel/friend/FriendFamilyApplication.java
@@ -2,8 +2,10 @@ package com.test.mytel.friend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class FriendFamilyApplication {
 
 	public static void main(String[] args) {
